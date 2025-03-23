8ba00a01ab0311dcbea598be3c60da87e0518241
chigorgor <ccasadu15@gmail.com>
2025-03-23 19:02:54 +0100

    set up top bar

---
+++ app/src/main/java/com/chichi/shippingapp/screens/home/TopAppBar.kt
@@ -0,0 +1,57 @@
+package com.chichi.shippingapp.screens.home
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import com.chichi.shippingapp.R
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TopBar(
+    isRootRoute: Boolean,
+    onBackClicked: () -> Unit
+) {
+
+    CenterAlignedTopAppBar(
+        title = {
+            Row (
+                verticalAlignment = Alignment.CenterVertically
+            ){
+
+                Text(
+                    text = stringResource(id = R.string.app_name),
+                    color = Color.White
+                )
+            }
+        },
+        navigationIcon = {
+            if (!isRootRoute) {
+                IconButton(
+                    onClick = onBackClicked
+                ) {
+                    Icon(
+                        imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                        tint = Color.White,
+                        contentDescription = "menu items"
+                    )
+                }
+            }
+        },
+
+        colors = TopAppBarDefaults.topAppBarColors(MaterialTheme.colorScheme.primary),
+
+
+    )
+}
